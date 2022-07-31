2c1c313ad2c431b16ef0117c45e75bfd8db65ebe
MatthewHardman <matthew9016@gmail.com>
2022-07-31 10:14:17 -0500

    Changed from broswerrouter to hashrouter to support gh pages hsoting

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Shop from "./components/Shop";
 import Homepage from "./components/Homepage";
 import NotFound from "./components/NotFound";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/Homepage" element={<Homepage />} />
         <Route path="/Shop/*" element={<Shop />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
